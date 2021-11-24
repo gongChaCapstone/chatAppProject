ef8db9f6d829dbb0e44d13277a1d50f08a5b2989
Sabi Kumar <skumar213@gmail.com>
2021-11-24 16:38:24 -0500

    updated authform

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -1,118 +1,87 @@
 import React from 'react'
-import {connect} from 'react-redux'
+import {useDispatch, useSelector} from 'react-redux'
 import {authenticate} from '../store'
 
-/**
- * COMPONENT
- */
-const AuthForm = props => {
-  const {name, displayName, handleSubmit, error} = props
+export const Login = () => {
+  const error = useSelector(state => state.auth.error)
+  const dispatch = useDispatch();
 
-  if (name === 'login') {
-    return (
-      <div>
-        <form onSubmit={handleSubmit} name={name}>
-          <div>
-            <label htmlFor="email">
-              <small>Email</small>
-            </label>
-            <input name="email" type="text" />
-          </div>
-          <div>
-            <label htmlFor="password">
-              <small>Password</small>
-            </label>
-            <input name="password" type="password" />
-          </div>
-          <div>
-            <button type="submit">{displayName}</button>
-          </div>
-          {error && error.response && <div> {error.response.data} </div>}
-        </form>
-      </div>
-    )
-  } else {
-    return (
-      <div>
-        <form onSubmit={handleSubmit} name={name}>
-          <div>
-            <label htmlFor="email">
-              <small>Email</small>
-            </label>
-            <input name="email" type="text" />
-          </div>
-          <div>
-            <label htmlFor="password">
-              <small>Password</small>
-            </label>
-            <input name="password" type="password" />
-          </div>
-          <div>
-            <label htmlFor="firstname">
-              <small>First Name</small>
-            </label>
-            <input name="firstname" type="text" />
-          </div>
-          <div>
-            <label htmlFor="lastname">
-              <small>Last Name</small>
-            </label>
-            <input name="lastname" type="text" />
-          </div>
-          <div>
-            <button type="submit">{displayName}</button>
-          </div>
-          {error && error.response && <div> {error.response.data} </div>}
-        </form>
-      </div>
-    )
+  const handleSubmit = (evt) => {
+    evt.preventDefault()
+    const email = evt.target.email.value
+    const password = evt.target.password.value
+    dispatch(authenticate(email, password, 'login'))
   }
 
-
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">
+            <small>Email</small>
+          </label>
+          <input name="email" type="text" />
+        </div>
+        <div>
+          <label htmlFor="password">
+            <small>Password</small>
+          </label>
+          <input name="password" type="password" />
+        </div>
+        <div>
+          <button type="submit">Login</button>
+        </div>
+        {error && error.response && <div> {error.response.data} </div>}
+      </form>
+    </div>
+  )
 }
 
-/**
- * CONTAINER
- *   Note that we have two different sets of 'mapStateToProps' functions -
- *   one for Login, and one for Signup. However, they share the same 'mapDispatchToProps'
- *   function, and share the same Component. This is a good example of how we
- *   can stay DRY with interfaces that are very similar to each other!
- */
-const mapLogin = state => {
-  return {
-    name: 'login',
-    displayName: 'Login',
-    error: state.auth.error
+export const Signup = () => {
+  const error = useSelector(state => state.auth.error)
+  const dispatch = useDispatch();
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault()
+    const email = evt.target.email.value
+    const password = evt.target.password.value
+    const firstname = evt.target.firstname.value
+    const lastname = evt.target.lastname.value
+    dispatch(authenticate(email, password, 'signup', firstname, lastname))
   }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">
+            <small>Email</small>
+          </label>
+          <input name="email" type="text" />
+        </div>
+        <div>
+          <label htmlFor="password">
+            <small>Password</small>
+          </label>
+          <input name="password" type="password" />
+        </div>
+        <div>
+          <label htmlFor="firstname">
+            <small>First Name</small>
+          </label>
+          <input name="firstname" type="text" />
+        </div>
+        <div>
+          <label htmlFor="lastname">
+            <small>Last Name</small>
+          </label>
+          <input name="lastname" type="text" />
+        </div>
+        <div>
+          <button type="submit">Sign Up</button>
+        </div>
+        {error && error.response && <div> {error.response.data} </div>}
+      </form>
+    </div>
+  )
 }
-
-const mapSignup = state => {
-  return {
-    name: 'signup',
-    displayName: 'Sign Up',
-    error: state.auth.error
-  }
-}
-
-const mapDispatch = dispatch => {
-  return {
-    handleSubmit(evt) {
-      evt.preventDefault()
-      const formName = evt.target.name
-      const email = evt.target.email.value
-      const password = evt.target.password.value
-      const firstname = evt.target.firstname ? evt.target.firstname.value : ''
-      const lastname = evt.target.lastname ? evt.target.lastname.value : ''
-
-      if (formName === 'signup') {
-        dispatch(authenticate(email, password, formName, firstname, lastname,))
-      } else {
-        dispatch(authenticate(email, password, formName))
-      }
-
-    }
-  }
-}
-
-export const Login = connect(mapLogin, mapDispatch)(AuthForm)
-export const Signup = connect(mapSignup, mapDispatch)(AuthForm)
