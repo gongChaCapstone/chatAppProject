5448448deb612110da4092b8633e7ce2f50077f6
Sabi Kumar <skumar213@gmail.com>
2021-11-30 17:45:15 -0500

    alllearning working

--- client/components/AllLearning.js
+++ client/components/AllLearning.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { fetchMaxTiers } from "../store/maxTiers"
+import { maxTier } from "./CompletionPage";
+
 
 const AllLearning = () => {
   const dispatch = useDispatch()
@@ -12,21 +14,18 @@ const AllLearning = () => {
   }, [])
 
 
-  const tierOne = 1 <= maxLearningTier ? <Link to='/learning/1'>Lesson One</Link> : <div>Lesson One</div>
-  const tierTwo = 2 <= maxLearningTier ? <Link to='/learning/2'>Lesson Two</Link> : <div>Lesson Two</div>
-  const tierThree = 3 <= maxLearningTier ? <Link to='/learning/3'>Lessone Three</Link> : <div>Lesson Three</div>
-  const tierFour = 4 <= maxLearningTier ? <Link to='/learning/4'>Lessone Four</Link> : <div>Lesson Four</div>
-  const tierFive = 5 <= maxLearningTier ? <Link to='/learning/5'>Lessone Five</Link> : <div>Lesson Five</div>
-  const tierSix = 6 <= maxLearningTier ? <Link to='/learning/6'>Lessone Six</Link> : <div>Lesson Six</div>
+  const allTiers = [];
+  for (let i = 1; i <= maxTier; i++) {
+    if (i <= maxLearningTier) {
+      allTiers.push(<div key={i}><Link to={`/learning/${i}`}>Lesson {i}</Link></div>)
+    } else {
+      allTiers.push(<div key={i}>Lesson {i}</div>)
+    }
+  }
 
   return (
     <div>
-      <div>{tierOne}</div>
-      <div>{tierTwo}</div>
-      <div>{tierThree}</div>
-      <div>{tierFour}</div>
-      <div>{tierFive}</div>
-      <div>{tierSix}</div>
+      {allTiers.map(tier => tier)}
     </div>
   );
 };
