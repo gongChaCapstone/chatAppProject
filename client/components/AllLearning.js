a07ecb1c0ac694d8574b022266bd7bb71acaf509
Sabi Kumar <skumar213@gmail.com>
2021-11-30 17:38:33 -0500

    fixed alllearning maxtier

--- client/components/AllLearning.js
+++ client/components/AllLearning.js
@@ -1,23 +1,19 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { fetchMaxTiers } from "../store/maxTiers"
 
 const AllLearning = () => {
   const dispatch = useDispatch()
+  const maxLearningTier = useSelector(state => state.maxTiers.highestLearningTier)
 
   useEffect(() => {
     dispatch(fetchMaxTiers())
   }, [])
 
-  useEffect(() => {
 
-  })
-
-  const maxLearningTier = useSelector((state) => {state.maxTiers})
-  console.log(maxLearningTier)
-  const tierOne = 1 <= maxLearningTier ? <Link to='/learning/1'>Lessone One</Link> : <div>Lesson One</div>
-  const tierTwo = 2 <= maxLearningTier ? <Link to='/learning/2'>Lessone Two</Link> : <div>Lesson Two</div>
+  const tierOne = 1 <= maxLearningTier ? <Link to='/learning/1'>Lesson One</Link> : <div>Lesson One</div>
+  const tierTwo = 2 <= maxLearningTier ? <Link to='/learning/2'>Lesson Two</Link> : <div>Lesson Two</div>
   const tierThree = 3 <= maxLearningTier ? <Link to='/learning/3'>Lessone Three</Link> : <div>Lesson Three</div>
   const tierFour = 4 <= maxLearningTier ? <Link to='/learning/4'>Lessone Four</Link> : <div>Lesson Four</div>
   const tierFive = 5 <= maxLearningTier ? <Link to='/learning/5'>Lessone Five</Link> : <div>Lesson Five</div>
