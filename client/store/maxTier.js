8f085a87031294c93c45ffa9cd95654dad44b8bd
Peter Oehman <tucker2923@gmail.com>
2021-11-30 16:34:21 -0500

    added axios thunk to get max tiers

--- client/store/maxTier.js
+++ client/store/maxTier.js
@@ -3,40 +3,31 @@ import { authenticateRequest } from './gateKeepingMiddleware'
 
 
 //action types
-const GET_TIER = 'GET_TIER';
+const GET_MAX_TIERS = 'GET_MAX_TIERS';
 
 //action creators
-const getTier = (tier) => {
+const setMaxTiers = (tiers) => {
   return {
-    type: GET_TIER,
-    tier
+    type: GET_MAX_TIERS,
+    tiers
   }
 }
 
 //thunk creators
-export const fetchPhrases = (tierId) => async (dispatch) => {
+export const fetchMaxTiers = () => async (dispatch) => {
   try {
-    const {data: currentPhrases} = await axios.get(`/api/phrases/${tierId}`)
-
-    dispatch(setPhrases(currentPhrases))
+    const maxTiers = await authenticateRequest('get', `/api/users/maxTier`)
+    dispatch(setMaxTiers(maxTiers))
   } catch (error) {
     console.log(error)
   }
 }
 
-export const unlockPhrases = (tierId) => async () => {
-  try {
-    await authenticateRequest('put', `/api/users/update/${tierId}`, {})
-  } catch (error) {
-    console.error(error)
-  }
-}
-
 //reducer
-export default function(state = [], action) {
+export default function(state = {}, action) {
   switch (action.type) {
-    case SET_PHRASES:
-      return action.phrases
+    case GET_MAX_TIERS:
+      return action.tiers
     default:
       return state
   }
