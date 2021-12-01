4f74277263ddf922b4bfb69f90192102f6a0afc6
Brendan Shulman <brendan.shulman@gmail.com>
2021-12-01 12:19:39 -0500

    completed quickstart

--- client/Routes.js
+++ client/Routes.js
@@ -1,40 +1,42 @@
-import React, {useEffect} from 'react'
-import {useDispatch, useSelector} from 'react-redux'
-import {withRouter, Route, Switch, Redirect} from 'react-router-dom'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { withRouter, Route, Switch, Redirect } from 'react-router-dom';
 import { Login, Signup } from './components/AuthForm';
 import Home from './components/Home';
-import {me} from './store'
+import { me } from './store';
 import SingleLearning from './components/SingleLearning';
 import CompletionPage from './components/CompletionPage';
-import AllLearning from './components/AllLearning'
+import AllLearning from './components/AllLearning';
+import QuickStartGuide from './components/QuickStartGuide';
 
 const Routes = () => {
-  const isLoggedIn = useSelector(state => !!state.auth.id)
+  const isLoggedIn = useSelector((state) => !!state.auth.id);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(me())
-  }, [])
+    dispatch(me());
+  }, []);
 
   return (
     <div>
       {isLoggedIn ? (
         <Switch>
-          <Route exact path='/allLearning' component={AllLearning}/>
-          <Route exact path ='/learning/:tier' component={SingleLearning}/>
-          <Route path='/completionPage' component={CompletionPage}/>
+          <Route exact path="/allLearning" component={AllLearning} />
+          <Route exact path="/learning/:tier" component={SingleLearning} />
+          <Route path="/completionPage" component={CompletionPage} />
           <Route path="/home" component={Home} />
+          <Route path="/quickstart" component={QuickStartGuide} />
           <Redirect to="/home" />
         </Switch>
       ) : (
         <Switch>
-          <Route path='/' exact component={ Login } />
+          <Route path="/" exact component={Login} />
           <Route path="/login" component={Login} />
           <Route path="/signup" component={Signup} />
         </Switch>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default withRouter(Routes)
+export default withRouter(Routes);
---
+++ client/components/QuickStartGuide.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const QuickStartGuide = () => {
+  return (
+    <div>
+      <h1>Quick Start Guide</h1>
+      <img src="QSG RedPandaASL.jpg" />
+    </div>
+  );
+};
+
+export default QuickStartGuide;
