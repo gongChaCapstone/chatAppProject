4d36c8831889c04bdbf77d831fb1325c5f2d9454
Brendan Shulman <brendan.shulman@gmail.com>
2021-12-01 17:02:41 -0500

    completed asl greeting

--- client/components/AllLearning.js
+++ client/components/AllLearning.js
@@ -1,34 +1,96 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import { fetchMaxTiers } from "../store/maxTiers"
-import { maxTier } from "./CompletionPage";
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchMaxTiers } from '../store/maxTiers';
+import { maxTier } from './CompletionPage';
 
+// To Do:
+// 1. add user points to top of page
+// 2. Add ASL greeting top of page
+const alphabet = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+];
 
 const AllLearning = () => {
-  const dispatch = useDispatch()
-  const maxLearningTier = useSelector(state => state.maxTiers.highestLearningTier)
+  const dispatch = useDispatch();
+  const maxLearningTier = useSelector(
+    (state) => state.maxTiers.highestLearningTier
+  );
+  const currentUser = useSelector((state) => state.auth);
+  // const UserName = currentUser.firstname || '';
+  console.log(currentUser.firstname);
 
   useEffect(() => {
-    dispatch(fetchMaxTiers())
-  }, [])
-
+    dispatch(fetchMaxTiers());
+  }, []);
 
   const allTiers = [];
   for (let i = 1; i <= maxTier; i++) {
     if (i <= maxLearningTier) {
-      allTiers.push(<div key={i}><Link to={`/learning/${i}`}>Lesson {i}</Link></div>)
+      allTiers.push(
+        <div key={i}>
+          <Link to={`/learning/${i}`}>Lesson {i}</Link>
+        </div>
+      );
     } else {
-      allTiers.push(<div key={i}>Lesson {i}</div>)
+      allTiers.push(<div key={i}>Lesson {i}</div>);
     }
   }
 
+  const letterImages = {};
+  for (let i = 0; i < alphabet.length; i++) {
+    letterImages[alphabet[i]] = `letter${alphabet[i]}.png`;
+  }
+
+  const aslName = [];
+  for (let i = 0; i < currentUser.firstname.length; i++) {
+    aslName.push(
+      <img
+        key={i}
+        src={letterImages[currentUser.firstname[i].toUpperCase()]}
+        style={{
+          width: 50,
+          height: 50,
+        }}
+      />
+    );
+  }
+  console.log(aslName);
   return (
     <div>
-      {allTiers.map(tier => tier)}
+      <div>
+        <h1>Hello, {currentUser.firstname}</h1>
+        <div>Hello, {aslName.map((image) => image)}</div>
+        <h4>Points: {currentUser.points}</h4>
+      </div>
+      {allTiers.map((tier) => tier)}
     </div>
   );
 };
 
 export default AllLearning;
-
