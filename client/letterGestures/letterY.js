8e3bd3d73d3eed31a66b00a36a02d7716b15c8cc
Sabi Kumar <skumar213@gmail.com>
2021-12-02 17:24:48 -0500

    done!

--- client/letterGestures/letterY.js
+++ client/letterGestures/letterY.js
@@ -10,7 +10,8 @@ for (let finger of [fp.Finger.Ring, fp.Finger.Middle, fp.Finger.Index]) {
   letterY.addCurl(finger, fp.FingerCurl.FullCurl, 1.0);
   letterY.addCurl(finger, fp.FingerCurl.HalfCurl, 0.9);
   letterY.addDirection(finger, fp.FingerDirection.VerticalUp, 1.0);
-  letterY.addDirection(finger, fp.FingerDirection.VerticalUp, 1.0);
+  letterY.addDirection(finger, fp.FingerDirection.DiagonalUpLeft, 1.0);
+  letterY.addDirection(finger, fp.FingerDirection.DiagonalUpRight, 1.0);
 }
 
 letterY.addCurl(fp.Finger.Pinky, fp.FingerCurl.NoCurl, 1.0);
