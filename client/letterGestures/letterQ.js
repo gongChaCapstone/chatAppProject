b657bb02ea9dc03659f24d81728557f856bf80fe
Sabi Kumar <skumar213@gmail.com>
2021-12-03 12:16:14 -0500

    fixed q

--- client/letterGestures/letterQ.js
+++ client/letterGestures/letterQ.js
@@ -12,7 +12,7 @@ letterQ.addDirection(
 );
 letterQ.addDirection(fp.Finger.Thumb, fp.FingerDirection.DiagonalDownLeft, 1.0);
 
-letterQ.addCurl(fp.Finger.Index, fp.FingerCurl.NoCurl, .9);
+letterQ.addCurl(fp.Finger.Index, fp.FingerCurl.NoCurl, 1);
 letterQ.addCurl(fp.Finger.Index, fp.FingerCurl.HalfCurl, 1);
 letterQ.addDirection(fp.Finger.Index, fp.FingerDirection.DiagonalDownLeft, 1.0);
 letterQ.addDirection(
@@ -20,11 +20,12 @@ letterQ.addDirection(
   fp.FingerDirection.DiagonalDownRight,
   1.0
 );
-letterQ.addDirection(fp.Finger.Index, fp.FingerDirection.HorizontalLeft, 0.9);
-letterQ.addDirection(fp.Finger.Index, fp.FingerDirection.HorizontalRight, 0.9);
+letterQ.addDirection(fp.Finger.Index, fp.FingerDirection.HorizontalLeft, .9);
+letterQ.addDirection(fp.Finger.Index, fp.FingerDirection.HorizontalRight, .9);
 
 for (let finger of [fp.Finger.Ring, fp.Finger.Pinky, fp.Finger.Middle]) {
   letterQ.addCurl(finger, fp.FingerCurl.HalfCurl, 1);
+  letterQ.addCurl(finger, fp.FingerCurl.FullCurl, 1);
   letterQ.addDirection(finger, fp.FingerDirection.HorizontalRight, 1.0);
   letterQ.addDirection(finger, fp.FingerDirection.HorizontalLeft, 1.0);
 }
