53d9abc2d7aa21ccae5f8b1a7ccea37a168a340b
Sabi Kumar <skumar213@gmail.com>
2021-12-09 11:50:32 -0500

    fixed single learning key

--- client/components/AllLearning.js
+++ client/components/AllLearning.js
@@ -99,9 +99,9 @@ const AllLearning = () => {
       <img class="absolute object-cover h-full w-full z-0 mt-12"src="background3.png"/>
       <div class="flex">
         <div class="grid z-0 grid-cols-2 flex-grow flex-wrap justify-items-center my-8 p-5 mt-16">
-          {allTiers.map((tier) => {
-            return tier[1] === true ? <button className="inline-block  rounded-lg shadow-lg bg-green-700 hover:bg-green-300 hover:-translate-y-0.5 transform transition text-white uppercase tracking-widest font-medium text-3xl border-solid border-2 border-white w-96 h-20 mx-48 mt-16">{tier}</button> :
-            <button className="inline-block px-4 py-1 rounded-lg shadow-lg bg-gray-500 line-through text-opacity-40 transform transition text-white mt-3 uppercase tracking-widest font-medium text-3xl border-solid border-2 border-white w-96 h-20 mx-48 mt-16">{tier}</button>
+          {allTiers.map((tier, index) => {
+            return tier[1] === true ? <button key={index}className="inline-block  rounded-lg shadow-lg bg-green-700 hover:bg-green-300 hover:-translate-y-0.5 transform transition text-white uppercase tracking-widest font-medium text-3xl border-solid border-2 border-white w-96 h-20 mx-48 mt-16">{tier}</button> :
+            <button key={index} className="inline-block px-4 py-1 rounded-lg shadow-lg bg-gray-500 line-through text-opacity-40 transform transition text-white mt-3 uppercase tracking-widest font-medium text-3xl border-solid border-2 border-white w-96 h-20 mx-48 mt-16">{tier}</button>
           })}
         </div>
       </div>
