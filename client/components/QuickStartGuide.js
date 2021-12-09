e6d7062da90f2d4734a9a897ca262932ebf31e03
Maxwell Boykin <maxericboykin@gmail.com>
2021-12-08 21:19:59 -0500

    QSG

--- client/components/QuickStartGuide.js
+++ client/components/QuickStartGuide.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 
 const QuickStartGuide = () => {
   return (
-    <div>
-      <img class="border-2 border-black w-7/12"src="infographic.png" />
+    <div class="grid grid-cols-2 flex space-x-8">
+      <img class="border-2 border-black w-11/12 m-auto" src="QSG.png" />
       <div>
-        <h4 class="font-bold" >Tips and Tricks for the best user experience:</h4>
-        <ul>
+        <h4 class="font-bold text-2xl my-4" >Tips and Tricks for the best user experience:</h4>
+        <ul class="text-xl">
           <li>• Be sure to allow camera access</li>
           <li>
             • Keep your hand in the middle of the screen while making the gestures
@@ -17,9 +17,9 @@ const QuickStartGuide = () => {
             mimic the gesture as closely as possible
           </li>
         </ul>
-        <h4 class="font-bold">When ready, click the button below to start learning!</h4>
+        <h4 class="font-bold font-bold text-xl mt-4">When ready, click the button below to start learning!</h4>
         <Link to="allLearning">
-          <button className="inline-block px-4 py-1 rounded-lg shadow-lg bg-green-700 hover:bg-green-300 hover:-translate-y-0.5 transform transition text-white mt-3 uppercase tracking-wider font-semibold text-sm border-solid border-2 border-black w-44 h-10">Begin Learning</button>
+          <button className="inline-block px-4 py-1 rounded-lg shadow-lg bg-purple-500 hover:bg-purple-300 hover:-translate-y-0.5 transform transition text-white mt-3 uppercase tracking-wider font-semibold text-sm border-solid border-2 border-black w-44 h-10">Begin Learning</button>
         </Link>
       </div>
     </div>
--- client/components/TestCompletionPage.js
+++ client/components/TestCompletionPage.js
@@ -6,8 +6,18 @@ const TestCompletionPage = props => {
 
   return (
     <div>
-      <div>Congrats!! You completed test {currentTestTier}</div>
-      <Link to="/allLearning">Click Here to go back to all lessons</Link>
+    <div class="flex justify-center">
+      <img class="w-8/12 h-8/12 justify-center"src="/congratulations_test.gif"></img>
+      </div>
+      <div class="flex">
+      <h1 class="text-4xl font-bold text-red-500 font-mono m-auto">You have completed <span class="text-purple-400"> test {currentTestTier}</span>!</h1>
+      </div>
+
+      <div class ="flex grid grid-cols-1 p-12 justify-items-center">
+        <button class="inline-block  rounded-lg shadow-lg bg-purple-500 hover:bg-purple-300 hover:-translate-y-0.5 transform transition text-white  tracking-widest font-medium text-xl border-solid border-2 border-white w-48 h-16 mx-48 mt-16">
+      <Link to="/allLearning">All lessons</Link> </button>
+
+    </div>
     </div>
   );
 };
